ee98829500dd3864421c155c63e94487489d6b50
Rickey <weems505@gmail.com>
2019-12-20 13:14:31 -0500

    Updated backend Customer components

--- BusyBankSpringBoot/src/main/java/pyramid/BusyBankSpringBoot/CustomerController.java
+++ BusyBankSpringBoot/src/main/java/pyramid/BusyBankSpringBoot/CustomerController.java
@@ -3,6 +3,8 @@ package pyramid.BusyBankSpringBoot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class CustomerController {
     @Autowired
@@ -16,5 +18,16 @@ public class CustomerController {
     @RequestMapping(value = "/delete/{id}",method = RequestMethod.DELETE)
     public void deleteCustomer(@PathVariable int id){
         cs.deleteCustomer(id);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Optional<Customer> getCustomer(@PathVariable int id){
+        return cs.getCustomer(id);
+
+    }
+
+    @RequestMapping(value = "/{id}/{name}",method = RequestMethod.PUT)
+    public void updateCustomer(@PathVariable int id, @PathVariable String name){
+        cs.addCustomer(cs.updateCustomer(id,name));
     }
 }
