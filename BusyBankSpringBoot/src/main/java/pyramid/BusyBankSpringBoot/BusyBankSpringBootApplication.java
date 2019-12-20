adfdbdc5da9328dd7f93f1dd2ecb0edab8bb8b7d
William <will3shu@gmail.com>
2019-12-20 10:25:09 -0500

    Busy Bank First

---
+++ BusyBankSpringBoot/src/main/java/pyramid/BusyBankSpringBoot/BusyBankSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pyramid.BusyBankSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyBankSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusyBankSpringBootApplication.class, args);
+	}
+
+}
